6d690eb94e6ab02ab1f527ba38d0b2c61493223e
r0binsahin <robin.sahin88@gmail.com>
2024-10-28 09:16:49 +0100

    list drinks by searchword

--- src/components/cocktail-list.ts
+++ src/components/cocktail-list.ts
@@ -1,7 +1,40 @@
-import { component } from 'haunted';
+import { component, useEffect, useState } from 'haunted';
 import { html } from 'lit-html';
+import { Cocktail } from '../types';
 
 const CocktailList = () => {
+  const [cocktails, setCocktails] = useState<Cocktail[]>([]);
+
+  const searchCocktails = async (query: string) => {
+    try {
+      const response = await fetch(
+        `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${query}`
+      );
+      const data = await response.json();
+      setCocktails(data.drinks || []);
+
+      if (data.drinks && data.drinks.length) {
+        console.log('res:', data.drinks);
+      } else {
+        console.log('something went wrong');
+      }
+    } catch (error) {
+      console.error('Search error:', error);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('search', ((e: CustomEvent) => {
+      searchCocktails(e.detail);
+    }) as EventListener);
+
+    return () => {
+      window.removeEventListener('search', ((e: CustomEvent) => {
+        searchCocktails(e.detail);
+      }) as EventListener);
+    };
+  }, []);
+
   return html`
     <style>
       .container {
@@ -12,7 +45,9 @@ const CocktailList = () => {
         gap: 20px;
       }
     </style>
-    <p>hej</p>
+    ${cocktails.map((cocktail) => {
+      return html` <div><p>${cocktail.strDrink}</p></div>`;
+    })}
   `;
 };
 
